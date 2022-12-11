4aacfa45b64492f21d7070f71f08ffb990882152
Sohrab Safi <sb.safi286@gmail.com>
2022-12-11 01:52:19 -0600

    KarateAssignment

---
+++ tek.karate.api.retail/src/test/java/api/test/TestRunner.java
@@ -0,0 +1,13 @@
+package api.test;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+	
+	@Karate.Test
+	public Karate runTest() {
+		return Karate.run("classpath:features").tags("");
+		
+	}
+
+}
